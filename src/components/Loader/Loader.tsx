03f8506a5059df4de12d34ce7813adbbbac9f7f1
jdhl27 <independientemedellin.27.03@gmail.com>
2024-08-02 19:19:10 -0500

    add Loader component with Lottie animation for loading states

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,23 @@
+import Lottie from "lottie-react";
+import LoaderLottie from "../../assets/animations/loader.json";
+import "./Loader.css";
+
+interface LoaderProps {
+  isLoading: boolean;
+}
+
+function Loader({ isLoading }: LoaderProps) {
+  if (!isLoading) return null;
+
+  return (
+    <div className="loader">
+      <Lottie
+        className="loader-lottie"
+        animationData={LoaderLottie}
+        loop={true}
+      />
+    </div>
+  );
+}
+
+export default Loader;
