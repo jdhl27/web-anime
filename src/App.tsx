4d1d973508b68ea85c7fc9a65e9f95785c6e50a9
jdhl27 <independientemedellin.27.03@gmail.com>
2024-08-02 21:09:08 -0500

    add call to anime search request

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { useEffect, useRef, useState } from "react";
+import { searchAnime } from "./api/anime";
 import "./App.css";
 import Loader from "./components/Loader/Loader";
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [isSearching, setIsSearching] = useState(false);
+
+  const [dataList, setDataList] = useState<Anime[] | []>([]);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [message, setMessage] = useState<string | null>("");
   useEffect(() => {
     handleSearch("");
   }, []);
@@ -11,6 +16,31 @@ function App() {
     // logic
     setIsSearching(true);
     setIsLoading(true);
+    // data
+    setDataList([]);
+    setMessage("");
+    setSearchTerm(searchTerm);
+    try {
+      const data = await searchAnime(searchTerm.trim().toLowerCase(), 1);
+      if (data.data.length === 0 && searchTerm !== "") {
+        setMessage(`No results found for "${searchTerm}"`);
+        return;
+      }
+
+      // insert value
+      setDataList(data.data);
+    } catch (error) {
+      setMessage(
+        `It's not you, it's us. It seems we have a problem on the server.`
+      );
+    } finally {
+      setTimeout(() => {
+        setIsLoading(false);
+        setIsSearching(false);
+      }, 500);
+    }
+  };
+
   };
 
   return (
