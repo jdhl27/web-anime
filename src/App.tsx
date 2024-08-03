909b4f3faba940791f879aab1a690f0adfb8dc07
jdhl27 <independientemedellin.27.03@gmail.com>
2024-08-02 21:06:08 -0500

    add method to search for an anime

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,23 @@ import "./App.css";
 import Loader from "./components/Loader/Loader";
 function App() {
   const [isLoading, setIsLoading] = useState(true);
+  const [isSearching, setIsSearching] = useState(false);
+  useEffect(() => {
+    handleSearch("");
+  }, []);
+  const handleSearch = async (searchTerm: string) => {
+    // logic
+    setIsSearching(true);
+    setIsLoading(true);
+  };
+
   return (
     <>
       <Loader isLoading={isLoading} />
+      <div className="container-center">
+        <Title />
+        <Input isSearching={isSearching} handleValueInput={handleSearch} />
+      </div>
     </>
   );
 }
